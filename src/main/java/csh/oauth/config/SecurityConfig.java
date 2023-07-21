c7d81bb00f93b513c22c1ba9c07e9ce50f702cae
csupreme19 <csupreme19@gmail.com>
2023-07-21 18:21:21 +0900

    커스텀 SecurityFilterChain 등록

---
+++ src/main/java/csh/oauth/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package csh.oauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        return http.build();
+    }
+
+}
