67dff324bade548b50d16a34803c70b6507707f5
csupreme19 <csupreme19@gmail.com>
2023-07-21 18:31:59 +0900

    폼 인증, Basic 인증 처리 등록

--- src/main/java/csh/oauth/config/SecurityConfig.java
+++ src/main/java/csh/oauth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package csh.oauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +14,13 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.exceptionHandling(handler -> {
+            handler.authenticationEntryPoint((request, response, authException) -> {
+                System.out.println("Custom AuthenticationEntryPoint");
+            });
+        });
         return http.build();
     }
 
