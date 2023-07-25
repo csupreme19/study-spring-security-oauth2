fa621093e0b260172d8771de07013969707b6c62
csupreme19 <csupreme19@gmail.com>
2023-07-25 15:55:35 +0900

    HTTP Basic 인증 살펴보기

--- src/main/java/csh/oauth/config/SecurityConfig.java
+++ src/main/java/csh/oauth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package csh.oauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,13 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+        ;
+
         http.headers(AbstractHttpConfigurer::disable);
         http.csrf(AbstractHttpConfigurer::disable);
         http.logout(AbstractHttpConfigurer::disable);
