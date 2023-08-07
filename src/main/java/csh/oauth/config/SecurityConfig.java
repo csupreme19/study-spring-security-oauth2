e825fa497d76b9d5a2179cf8f985ae69ce34caaf
csupreme19 <csupreme19@gmail.com>
2023-08-07 17:42:18 +0900

    OAuth 2.0 Client Registration, Provider 설정

--- src/main/java/csh/oauth/config/SecurityConfig.java
+++ src/main/java/csh/oauth/config/SecurityConfig.java
@@ -1,52 +1,8 @@
 package csh.oauth.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-        ;
-
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        ;
-
-        http.headers(AbstractHttpConfigurer::disable);
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.logout(AbstractHttpConfigurer::disable);
-        http.requestCache(AbstractHttpConfigurer::disable);
-        http.securityContext(AbstractHttpConfigurer::disable);
-        http.anonymous(AbstractHttpConfigurer::disable);
-        http.exceptionHandling(AbstractHttpConfigurer::disable);
-        return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
-        configurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return configurationSource;
-    }
 
 }
