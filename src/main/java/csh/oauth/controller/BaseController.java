d1f05e981f4a1d48c27f77d86125c9d8f13e0ea7
csupreme19 <csupreme19@gmail.com>
2023-08-07 19:11:35 +0900

    ClientRegistrationRepository를 통한 Registration 클라이언트 정보 수동 등록

--- src/main/java/csh/oauth/controller/BaseController.java
+++ src/main/java/csh/oauth/controller/BaseController.java
@@ -1,14 +1,24 @@
 package csh.oauth.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class BaseController {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping("/")
     public String index() {
-        return "hello world";
+        ClientRegistration keycloak = clientRegistrationRepository.findByRegistrationId("keycloak");
+        String clientId = keycloak.getClientId();
+        String clientSecret = keycloak.getClientSecret();
+        String clientName = keycloak.getClientName();
+        return String.format("Hello %s(%s, %s)!", clientName, clientId, clientSecret);
     }
 
 }
