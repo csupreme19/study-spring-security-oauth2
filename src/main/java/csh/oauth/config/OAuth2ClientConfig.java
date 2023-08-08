bb96921bc591f819b2cbdc7e0918767f5f81b4ea
csupreme19 <csupreme19@gmail.com>
2023-08-08 16:48:54 +0900

    OAuth2 Logout Handler

--- src/main/java/csh/oauth/config/OAuth2ClientConfig.java
+++ src/main/java/csh/oauth/config/OAuth2ClientConfig.java
@@ -1,41 +1,38 @@
 package csh.oauth.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
+@RequiredArgsConstructor
 public class OAuth2ClientConfig {
 
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
-
-    private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistrations.fromOidcIssuerLocation("http://localhost:8080/realms/oauth2")
-                .registrationId("keycloak")
-                .clientId("oauth2-client-app")
-                .clientSecret("vO5lZrIFT07yCF6pM9uiW4TSMEJKEtDf")
-                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-                .scope("openid")
-                .build();
-    }
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(request -> request.requestMatchers("/loginPage").permitAll()
-//                .anyRequest().authenticated());
-        http.authorizeHttpRequests(request -> request.anyRequest().permitAll());
-        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
-        http.oauth2Client(Customizer.withDefaults());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.logout(logout -> {
+            logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .deleteCookies("JSESSIONID");
+        });
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+        return successHandler;
     }
 
 }
