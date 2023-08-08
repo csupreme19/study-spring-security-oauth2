a648b5203a7987aefcac2624c0ba124ce31d8c99
csupreme19 <csupreme19@gmail.com>
2023-08-08 11:21:29 +0900

    기본 로그인 페이지 DefaultLoginGeneratingFilter 살펴보기

--- src/main/java/csh/oauth/controller/BaseController.java
+++ src/main/java/csh/oauth/controller/BaseController.java
@@ -1,8 +1,6 @@
 package csh.oauth.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BaseController {
 
-    private final ClientRegistrationRepository clientRegistrationRepository;
+//    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping("/")
     public String index() {
-        ClientRegistration keycloak = clientRegistrationRepository.findByRegistrationId("keycloak");
-        String clientId = keycloak.getClientId();
-        String clientSecret = keycloak.getClientSecret();
-        String clientName = keycloak.getClientName();
-        return String.format("Hello %s(%s, %s)!", clientName, clientId, clientSecret);
+//        ClientRegistration keycloak = clientRegistrationRepository.findByRegistrationId("keycloak");
+//        String clientId = keycloak.getClientId();
+//        String clientSecret = keycloak.getClientSecret();
+//        String clientName = keycloak.getClientName();
+//        return String.format("Hello %s(%s, %s)!", clientName, clientId, clientSecret);
+        return "hello";
     }
 
 }
