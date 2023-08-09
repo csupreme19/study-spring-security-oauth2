677f62e450b082e27c8b00d5c21efdf98bf7c757
csupreme19 <csupreme19@gmail.com>
2023-08-09 15:49:50 +0900

    JwtDecoder 수동 등록(issuer-uri)

--- src/main/java/csh/oauth/config/OAuth2ResourceServerConfig.java
+++ src/main/java/csh/oauth/config/OAuth2ResourceServerConfig.java
@@ -1,9 +1,12 @@
 package csh.oauth.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,11 +15,18 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @RequiredArgsConstructor
 public class OAuth2ResourceServerConfig {
 
+    private final OAuth2ResourceServerProperties properties;
+
     @Bean
     public SecurityFilterChain oauth2ResourceServer(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
         http.oauth2ResourceServer(config -> config.jwt(withDefaults()));
+//        http.oauth2ResourceServer(config -> config.opaqueToken(withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
     }
 
 }
